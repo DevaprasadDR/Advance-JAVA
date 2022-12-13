b0e1f2d5f47061a229adccf03a836394097725a2
DevaprasadDR <deva.prasad@qualitestgroup.com>
2022-12-13 16:31:39 +0530

    added factorial

---
+++ src/main/java/Programs/factorial.java
@@ -0,0 +1,13 @@
+package Programs;
+
+public class factorial {
+    public static void main(String[] args) {
+        int n=6;
+        int fact=1;
+        for (int i=n;i>=1; i--)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
