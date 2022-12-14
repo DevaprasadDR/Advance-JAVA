c4e179a368a8aae7704accb2a1297e1656cb3c64
DevaprasadDR <deva.prasad@qualitestgroup.com>
2022-12-14 18:02:49 +0530

    added parmeter

---
+++ src/main/java/Parameters/Triange_pattern.java
@@ -0,0 +1,27 @@
+package Parameters;
+
+import java.util.Scanner;
+
+public class Triange_pattern {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row");
+        int row = sc.nextInt();
+        triangle(row);
+    }
+    static void triangle(int row)
+    {
+        int i,j;
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
