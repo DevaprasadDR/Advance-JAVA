f03fd89d5aeffba44cd959e5d2342388adb03a40
QUALITEST\deepak.dhas <deepak.dhas@qualitestgroup.com>
2022-12-23 13:44:27 +0530

    test

--- src/main/java/ExcelWriter/TestCase.java
+++ src/main/java/ExcelWriter/TestCase.java
@@ -4,25 +4,21 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Writeexcel {
-    public void readExcel(String filePath,String fileName,String sheetName) throws IOException{
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
+public class TestCase extends ExcelUtilities {
+    public static void main(String [] args) throws IOException {
 
-        XSSFSheet sheet = workbook.createSheet(sheetName);
+        ExcelUtilities objExcelFile = new ExcelUtilities();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements you want to store: ");
         int n = sc.nextInt();
         Scanner sc1 = new Scanner(System.in);
         System.out.println("Enter the row");
-        XSSFRow row1 = sheet.createRow(sc1.nextInt());
         Scanner sc2 = new Scanner(System.in);
-
 
         int[] array = new int[10];
         System.out.println("Enter the data to store in a cell: ");
@@ -34,13 +30,13 @@ public class Writeexcel {
 
         for (int i=0; i<n; i++)
         {
-            row1.createCell(sc1.nextInt()).setCellValue(array[i] );
+            for (int j=0;j<n; j++)
+            objExcelFile.writeExcelString("Hello","hello1",i,j, array[i]);
+
         }
-        FileOutputStream file = new FileOutputStream(filePath+"/"+fileName);
-        workbook.write(file);
+
     }
-        }
 
-
+}
 
 
