589ab6a5e899775cb556c338e923155c23f30e91
Kaviya.V <Kaviya.V@qualitestgroup.com>
2022-12-26 12:00:45 +0530

    commit

--- src/main/java/ExcelWriter/TestCase_Kaviya.java
+++ src/main/java/ExcelWriter/TestCase_Kaviya.java
@@ -1,29 +1,30 @@
 package ExcelWriter;
 
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
-import java.io.FileInputStream;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
-public class TestCase_Kaviya {
+public class TestCase_Kaviya extends ExcelUtilities {
 
     public static void main(String[] args) throws Exception {
         ExcelUtilities objExcelFile = new ExcelUtilities();
 
-//        FileInputStream inp = new FileInputStream("C:\\Users\\Kaviya.V\\IdeaProjects\\Advanced_Java\\Advance-JAVA\\src\\datafiles\\ReadData.xlsx" + ""))
-//        {
-//            Workbook wb = WorkbookFactory.create(inp);
-//            Sheet sheet = wb.getSheetAt(0);
-//            Scanner sc1 = new Scanner(System.in);
-//            System.out.println("Enter x");
-//            Row x = sheet.getRow(sc1.nextInt());
-//            Scanner sc2 = new Scanner(System.in);
-//            System.out.println("Enter y: ");
-//            Cell y = x.getCell(sc2.nextInt());
-//            if (y != null)
-//                System.out.println("Data: " + y);
-//            else
-//                System.out.println("Cell is empty");
+        /*FileInputStream inp = new FileInputStream("C:\\Users\\Kaviya.V\\IdeaProjects\\Advanced_Java\\Advance-JAVA\\src\\datafiles\\ReadData.xlsx" + ""))
+        {
+            Workbook wb = WorkbookFactory.create(inp);
+            Sheet sheet = wb.getSheetAt(0);*/
+           Scanner sc1 = new Scanner(System.in);
+           System.out.println("Enter x");
+        Row x = sheet.getRow(sc1.nextInt());
+           Scanner sc2 = new Scanner(System.in);
+            System.out.println("Enter y: ");
+            Cell y = x.getCell(sc2.nextInt());
+            if (y != null)
+                System.out.println("Data: " + y);
+           else
+                System.out.println("Cell is empty");
 
             int n=0;
             String Value = "";
@@ -34,4 +35,4 @@ public class TestCase_Kaviya {
             }
         }
     }
-}
+
