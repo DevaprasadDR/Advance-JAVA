9e307ff456f02e3236466e4a7638d16f434da62c
sathiyapriya.s <sathiyapriya.s@qualitestgroup.com>
2023-01-04 12:47:50 +0530

    collections

---
+++ src/main/java/Collections/Sathiyapriya/ArrayProgram.java
@@ -0,0 +1,24 @@
+package Collections.Sathiyapriya;
+
+import java.util.Scanner;
+
+public class ArrayProgram {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the length of the string array: ");
+        int n=sc.nextInt();
+        String[] value=new String[n];
+        System.out.print("Enter the string");
+        for(int i=0; i<n; i++)
+        {
+            value[i]=sc.next();
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Value stored in "+i+" index is "+value[i]);
+        }
+
+    }
+}
+
+
