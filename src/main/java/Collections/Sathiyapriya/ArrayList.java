f323dd6b8a6fae78477b993c4817bc90b7f45c14
sathiyapriya.s <sathiyapriya.s@qualitestgroup.com>
2023-01-04 13:10:40 +0530

    collections

---
+++ src/main/java/Collections/Sathiyapriya/ArrayList.java
@@ -0,0 +1,23 @@
+package Collections.Sathiyapriya;
+
+import java.util.Scanner;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        java.util.ArrayList<Integer> list = new java.util.ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Which index should be removed");
+        list.remove(sc1.nextInt());
+        System.out.println(list);
+    }
+
+}
+
