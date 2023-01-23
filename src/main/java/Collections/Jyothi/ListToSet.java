14cdeeaac811ba12ac4cfd4be8ecd0298121dea6
Jyothi.Vuppalapaati <jyothi.vuppalapaati@qualitestgroup.com>
2023-01-23 13:56:34 +0530

    programs

--- src/main/java/Collections/Jyothi/ListToSet.java
+++ src/main/java/Collections/Jyothi/ListToSet.java
@@ -7,18 +7,27 @@ public class ListToSet {
 
 
         List<String> ll = new LinkedList<String>();
-        ll.add("Jyothi");
-        ll.add("Kaviya");
-        ll.add("Sathiya");
-        ll.add("Kaviya");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of values");
+        int num=in.nextInt();
+        Scanner sc1=new Scanner(System.in);
+
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("add elements");
+            String b = sc1.next();
+
+            ll.add(b);}
+
         for (String name : ll)
 
-            System.out.println(name);
+           System.out.println(name);
         Set<String> sm = new HashSet<>();
         sm.addAll(ll);
         System.out.println("Sorted list name:");
         for (String name : sm)
             System.out.println(name);
+
     }
 }
 
